8cc44d09f7459efd9723f97ac86c1b5d14d5a7d7
Jussi Ristimäki <rutle4@gmail.com>
2019-06-16 16:25:32 +0300

    Small code cleanup.

--- mainwindow.cc
+++ mainwindow.cc
@@ -19,6 +19,7 @@ MainWindow::MainWindow(QWidget *parent) :
     //QObject::connect(ui->catComboBox, static_cast<void(QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
     //                 ui->searchStackedWidget, &QStackedWidget::setCurrentIndex);
 
+
 }
 
 MainWindow::~MainWindow()
@@ -38,61 +39,27 @@ void MainWindow::updateData() // fetchData
 
         for (auto cat : program_->getModel()->getSelectedCats()) {
             //Program::SearchListDelegate *delegate{new Program::SearchListDelegate()};
+            QListView *tmp{new QListView()};
+            Program::SearchProxyModel *filterModel{new Program::SearchProxyModel(tmp)};
+            Program::SearchListModel *model = nullptr;
             if (cat == "Mods") {
-                QListView *tmp{new QListView()};
-                Program::SearchProxyModel *filterModel{new Program::SearchProxyModel(tmp)};
-                //std::make_shared<Program::SearchProxyModel>(filterModel);
-                //qDebug() << "Mods";
-                Program::SearchListModel *model{
-                    new Program::SearchListModel(
-                                program_->getModel()->getModData(), tmp)};
-                //catTOmodel_.insert("Mods", model);
-                ui->catComboBox->addItem("Mods");
-                filterModel->setSourceModel(model);
-
-                tmp->setModel(filterModel);
-                comboCats_.insert("Mods", filterModel);
-                searchListViews_.insert("Mods", tmp);
-                ui->searchStackedWidget->addWidget(searchListViews_.value("Mods"));
+                model = new Program::SearchListModel(
+                            program_->getModel()->getModData(), tmp);
             } else if(cat == "Primes") {
-                //qDebug() << "Primes";
-                QListView *tmp{new QListView()};
-                Program::SearchProxyModel *filterModel{new Program::SearchProxyModel(tmp)};
-                Program::SearchListModel *model{
-                    new Program::SearchListModel(
-                                program_->getModel()->getPrimeData(), tmp)};
-                //catTOmodel_.insert("Primes", model);
-                ui->catComboBox->addItem("Primes");
-                //qDebug() << "End primes";
-                filterModel->setSourceModel(model);
-                filterModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
-                tmp->setModel(filterModel);
-                comboCats_.insert("Primes", filterModel);
-                searchListViews_.insert("Primes", tmp);
-                ui->searchStackedWidget->addWidget(searchListViews_.value("Primes"));
+                model = new Program::SearchListModel(
+                                program_->getModel()->getPrimeData(), tmp);
             }
+            ui->catComboBox->addItem(cat);
+            filterModel->setSourceModel(model);
+            filterModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+            tmp->setModel(filterModel);
+            comboCats_.insert(cat, filterModel);
+            searchListViews_.insert(cat, tmp);
+            ui->searchStackedWidget->addWidget(searchListViews_.value(cat));
         }
         setSignals();
 
     }
-    //Program::SearchListModel *model{new Program::SearchListModel(program_->getModel()->getModData(), ui->modListView)};
-    //my_program::widgets::TopGamesListDelegate *delegate{new my_program::widgets::TopGamesListDelegate()};
-    //ui->modListView->setModel(model);
-    //topGamesList->setItemDelegate(delegate);
-    //ui->modListView->setVisible(true);
-    //topGamesList->setStyleSheet("QListView {background-color: transparent;}");
-
-    //QObject::disconnect(ui->searchEdit, &QLineEdit::textChanged,
-    //                   nullptr, nullptr);
-    //
-    //QObject::connect(ui->searchEdit, &QLineEdit::textChanged,
-    //                 [=](const QString &newValue){
-    //    searchText(newValue);
-    //    qDebug() << newValue;
-    //    });
-
-
-    //updateSignal();
 }
 
 void MainWindow::setSignals()
